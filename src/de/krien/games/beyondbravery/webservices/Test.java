b5cf943a4bd1fe0b8bf9bfb14b7f046ddfaa0426
Alex Krien <alexkrien@googlemail.com>
2017-02-02 09:16:00 +0100

    Init

--- src/de/krien/games/beyondbravery/webservices/Test.java
+++ src/de/krien/games/beyondbravery/webservices/Test.java
@@ -1,5 +1,8 @@
 package de.krien.games.beyondbravery.webservices;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -8,11 +11,20 @@ import javax.ws.rs.core.Response;
 @Path("/test")
 public class Test {
 
+	@SuppressWarnings("serial")
+	private static List<String> heroes = new ArrayList<String>() {{
+		add("Hero1");
+		add("Hero2");
+	}};
+	
 	@GET
 	@Path("/{param}")
 	public Response getMsg(@PathParam("param") String msg) {
-		String output = "Jersey say : " + msg;
-		return Response.status(200).entity(output).build();
+		StringBuilder stringBuilder = new StringBuilder();
+		for(String hero : heroes) {
+			stringBuilder.append(hero);
+		}
+		return Response.status(200).entity(stringBuilder.toString()).build();
 	}
 
 }
